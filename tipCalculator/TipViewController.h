152671d500a77cc2e290f5a017ad3430f6a5d150
Biren Barodia <bbarodia@yahoo-inc.com>
2015-01-20 13:10:55 -0800

    first commit

---
+++ tipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipCalculator
+//
+//  Created by Biren Barodia on 1/20/15.
+//  Copyright (c) 2015 Biren Barodia. All rights reserved.
+//
+
+#ifndef tipCalculator_TipViewController_h
+#define tipCalculator_TipViewController_h
+
+
+#endif
---
+++ tipCalculator/TipViewController.m
@@ -0,0 +1,9 @@
+//
+//  TipViewController.m
+//  tipCalculator
+//
+//  Created by Biren Barodia on 1/20/15.
+//  Copyright (c) 2015 Biren Barodia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
