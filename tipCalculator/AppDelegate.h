309a98ddc03ca9ce5eed36d5bb80664f39dd0183
Biren Barodia <bbarodia@yahoo-inc.com>
2015-01-19 21:56:21 -0800

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Biren Barodia on 1/19/15.
+//  Copyright (c) 2015 Biren Barodia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Biren Barodia on 1/19/15.
+//  Copyright (c) 2015 Biren Barodia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
